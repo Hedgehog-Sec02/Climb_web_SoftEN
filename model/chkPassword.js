cb5d60d4ff1e4acf6c751a24adba9107d4cf601e
Hedgehog-Sec02 <hedgehog.sec02@gmail.com>
2018-03-17 17:55:46 +0700

    update_chk_pass

--- model/chkPassword.js
+++ model/chkPassword.js
@@ -1,15 +1,15 @@
+
 function chkLeastPassword(){
     var pass1 = document.getElementById('Password');
     var pass2 = document.getElementById('con-Password');
     var message = document.getElementById('error-al');
     var message2 = document.getElementById('error-al2');
-    var regex = new RegExp() ; 
     // Password ---------------------------------------------------
     if(pass1.value != ''){
         if(pass1.value.length > 15){
                 pass1.style.backgroundColor =goodColor ;
                 message.style.color = goodColor ;
-                message.innerHTML = "Password is o!!" ; 
+                message.innerHTML = "Password is Valiable!!" ; 
         
         }else {
             pass1.style.backgroundColor = badColor;
@@ -31,7 +31,7 @@ function chkLeastPassword(){
         }else {
             pass2.style.backgroundColor = badColor ;
             message2.style.color = badColor ;
-            message2.innerHTML = "Password isn't match !!";
+            message2.innerHTML = "Password isn't match z!!";
         }
     }else {
         pass2.style.backgroundColor = "" ;
--- registration.php
+++ registration.php
@@ -8,7 +8,7 @@
     <meta charset="utf-8" />
 
     <script type="text/javascript" src="model/chkPassword.js"></script>
-    
+
     <!-- JS -->     
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
