57458ae7b53256189f6ebaa71c541dc5d4acac7a
Hedgehog-Sec02 <hedgehog.sec02@gmail.com>
2018-03-17 18:30:11 +0700

    update_chk_pass

--- model/chkPassword.js
+++ model/chkPassword.js
@@ -1,20 +1,20 @@
-
 function chkLeastPassword(){
     var pass1 = document.getElementById('Password');
     var pass2 = document.getElementById('con-Password');
     var message = document.getElementById('error-al');
     var message2 = document.getElementById('error-al2');
+    var pattern = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[\-])(?=.*[\_])[0-9a-zA-Z\-\_]{16,}$/;
+    
     // Password ---------------------------------------------------
     if(pass1.value != ''){
-        if(pass1.value.length > 15&&pass1.value.length <20){
-                pass1.style.backgroundColor =goodColor ;
-                message.style.color = goodColor ;
-                message.innerHTML = "Password is Valid ok !!" ; 
-        
+        if(pass1.value.match(pattern)){
+            pass1.style.backgroundColor =goodColor ;
+            message.style.color = goodColor ;
+            message.innerHTML = "Password is ok !!" ; 
         }else {
             pass1.style.backgroundColor = badColor;
             message.style.color = badColor;
-            message.innerHTML = " you have to enter at least 16 digit!";
+            message.innerHTML = " Password must contain at least (A-Z,a-z,0-9, _ , -) and 16 digit!";
             return;
         }
     }else {
@@ -31,12 +31,7 @@ function chkLeastPassword(){
         }else {
             pass2.style.backgroundColor = badColor ;
             message2.style.color = badColor ;
-            message2.innerHTML = "Password isn't match z!!";
+            message2.innerHTML = "Password isn't match !!";
         }
-    }else {
-        pass2.style.backgroundColor = "" ;
-        message2.style.color = "" ;
-        message2.innerHTML = '';
-        
     }
-}
+    }
--- registration.php
+++ registration.php
@@ -385,46 +385,7 @@
                     }); 
                 
             });
-<<<<<<< HEAD
-            function chkLeastPassword(){
-                var pass1 = document.getElementById('Password');
-                var pass2 = document.getElementById('con-Password');
-                var goodColor = "#66cc66";
-                var badColor = "#ff6666";
-                var message = document.getElementById('error-al');
-                var message2 = document.getElementById('error-al2');
-                var pattern = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[\-])(?=.*[\_])[0-9a-zA-Z\-\_]{16,}$/;
-                
-                // Password ---------------------------------------------------
-                if(pass1.value != ''){
-                    if(pass1.value.match(pattern)){
-                        pass1.style.backgroundColor =goodColor ;
-                        message.style.color = goodColor ;
-                        message.innerHTML = "Password is ok !!" ; 
-                    }else {
-                        pass1.style.backgroundColor = badColor;
-                        message.style.color = badColor;
-                        message.innerHTML = " Password must contain at least (A-Z,a-z,0-9, _ , -) and 16 digit!";
-                        return;
-                    }
-                }else {
-                    pass1.style.backgroundColor = "";
-                    message.style.color = "";
-                    message.innerHTML = "";
-                }
-                // Con-Password ------------------------------------------------
-                if(pass2.value != ''){
-                    if(pass1.value == pass2.value ){
-                        pass2.style.backgroundColor = goodColor ;
-                        message2.style.color = goodColor ;
-                        message2.innerHTML = "Password is match !!";
-                    }else {
-                        pass2.style.backgroundColor = badColor ;
-                        message2.style.color = badColor ;
-                        message2.innerHTML = "Password isn't match !!";
-                    }
-=======
-
+           
             var goodColor = "#66cc66";
             var badColor = "#ff6666";
 
@@ -432,7 +393,6 @@
                 var s = String.fromCharCode( e.which );
                 if ( s.toUpperCase() === s && s.toLowerCase() !== s && !e.shiftKey ) {
                     document.getElementById('alert-cap').style.visibility = 'visible';
->>>>>>> cb5d60d4ff1e4acf6c751a24adba9107d4cf601e
                 }else {
                     document.getElementById('alert-cap').style.visibility = 'hidden';
                 }
